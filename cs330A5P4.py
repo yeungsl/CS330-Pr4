7b0a86f2a51daeb61876f9e32f62e7bcd9cf0008
Sailung Yeung <yeungSL@wireless1x-155-41-86-129.bu.edu>
2016-03-31 13:33:14 -0400

    corrected the part a

--- cs330A5P4.py
+++ cs330A5P4.py
@@ -17,27 +17,18 @@ import copy
 import time
 import heapq
 ##(a)
-def algorithm1(a):
-    
-    print("For algorithm 1:")
-    
-    startTime = time.clock() 
+def findIfPartition(a):
+    t = time.clock()
     s = sum(a)
-    operation = 0  # counter for operation
-    
-    # handle odd sum
+    o = 0
     if s % 2 != 0:
         s -= 1
-    
-    # initialize 2d array with all 0s    
     sub = np.zeros((s//2 + 1, len(a)+1))
     for i in range(len(a)+1):
         sub[0][i] = 1
-        operation += 1
-     
-    r = 0    # r is the break point in the array
-    residue = "xxx"
+        o += 1
     
+    r = 0
     for i in range(1, s//2 + 1):
         for j in range(1, len(a)+1):
             sub[i][j] = sub[i][j-1]
@@ -48,49 +39,42 @@ def algorithm1(a):
             if i == s//2 and sub[i][j] == 1:
                 r = j
                 break
-            operation += 1
+            o += 1
         
-    print(sub)
-    print("r:", r)
-    if sum(a[0:r]) == s//2:
-        print("A1: ", a[0:r], " A2: ", a[r:],"\n")
-        residue = sum(a[0:r])-sum(a[r:])
-        
+    #print(sub)
+    #print("r:", r)
+    hs = s//2
+    mins = abs(sum(a[0:r]) - hs)
+   
+    if sum(a[0:r]) == hs:
+        A1 = a[0:r]
+        A2 = a[r:]
+        #print("A1: ", a[0:r], " A2: ", a[r:],"\n")
     else:
-        for i in range(len(a[0:r])):
-            operation += 1
-            d = copy.deepcopy(a[0:r])
-            del d[i]
-            if sum(d) == s//2:
-                print("A1: ",d," A2: ",a[r:]+[a[i]],"\n" )
-                residue = sum(d)-sum(a[r:]+[a[i]])
-                break
-                
-#            for j in range(len(a[0:r])):
-#                operation += 1
-#                
-#                #print("befor",d)
-#                del d[j]
-#                #print("after",d)
-#                if sum(d) == s//2:
-#                    print("A1: ",d," A2: ",a[r:]+[a[i],a[j]],"\n" )
-#                    residue = sum(d)-sum(a[r:]+[a[i]])
-#                    break break
-#                
-#                for k in range(len(a[0:r])):
-#                    operation += 1
-#                    del d[k]
-#                    if sum(d) == s//2:
-#                        print("A1: ",d," A2: ",a[r:]+[a[i],a[j],a[k]],"\n" )
-#                        residue = sum(d)-sum(a[r:]+[a[i]])
-#                        break break break
-            
-    runtime = time.clock() - startTime
-    print("The residue is: ",residue)
-    print("Running time: ", runtime,"s")
-    print("# of Operation: ", operation, " = m * n, where m is the size of the input array and n is the array sum/2")
-    print()
-    return runtime
+        
+        #print("initial:", mins)
+        for i in range(1, len(a[0:r])):
+            o += 1
+            d = copy.deepcopy(a[0:i])
+            d += [a[r - 1]]
+            #print("d:",d)
+            if abs(sum(d) - hs) < mins:
+                mins = abs(sum(d) - hs)
+                #print("min:", mins)
+                j = i
+                #print("min index:", j)
+        A1 = a[0:j] + [a[r-1]]
+        A2 = a[r:] + a[j :r-1]
+    
+    
+    S = [a * -1 for a in A1] + A2
+    t1 = time.clock() - t
+    print("residual: ", mins)
+    print("A1: ",A1," A2: ",A2, )
+    print("S:", S)
+    print("Running time: ", t1,"s")
+    print("# of Operation: ", o, " = m * n, where m is the size of the input array and n is the array sum/2")
+    return t1
 
 ##(b)
 def kk(a):
